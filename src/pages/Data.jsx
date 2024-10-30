1c3f58a90bede09e7dde4420587874b8b3c2cd52
ALPACINO400 <=maqatlan@gmail.com>
2024-10-30 09:21:24 +0300

    insital add the routing

---
+++ src/pages/Data.jsx
@@ -0,0 +1,32 @@
+// export const ProductData = [
+//   {
+//     id: 1,
+//     ProductTitle: "small apartment",
+//     ProductLocation: "Makkah",
+//     ProductDescription: "small apartment with one kitchen and bathroom",
+//     ProductImage:
+//       "https://i.pinimg.com/enabled_lo/564x/fc/e8/19/fce8195e506066cff3c05c112dd780bd.jpg",
+//     ProductPrice: 1109.95,
+//     ProductType: "rent",
+//   },
+//   {
+//     id: 2,
+//     ProductTitle: "big apartment",
+//     ProductLocation: "Riyadh",
+//     ProductDescription: "big apartment with 2  kitchen and 2 bathroom 1 pool",
+//     ProductImage:
+//       "https://i.pinimg.com/736x/f8/a7/11/f8a7119122e93036f93608c7cf299752.jpg",
+//     ProductPrice: 2109.95,
+//     ProductType: "sale",
+//   },
+//   {
+//     id: 3,
+//     ProductTitle: "mid apartment",
+//     ProductLocation: "jeddah",
+//     ProductDescription: "mid apartment with 1  kitchen and 1 bathroom 1 pool ",
+//     ProductImage:
+//       "	https://i.pinimg.com/736x/e6/b4/c9/e6b4c90ab804ecc52e18cff491179095.jpg",
+//     ProductPrice: 990,
+//     ProductType: "rent",
+//   },
+// ];
