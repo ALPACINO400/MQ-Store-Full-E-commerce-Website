b476385631aee33f49322981e29644f3a05a45bb
ALPACINO400 <=maqatlan@gmail.com>
2024-11-22 12:29:47 +0300

    footer fix1

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -37,6 +37,46 @@
 
 
 
+// import React from "react";
+// import { Box, Typography, Link } from "@mui/material";
+
+// const Footer = () => {
+//   return (
+//     <Box
+//       component="footer"
+//       sx={{
+//         backgroundColor: "#333",
+//         color: "#fff",
+//         padding: "20px",
+//         textAlign: "center",
+//         mt: 4,
+//       }}
+//     >
+//       <Typography variant="body2" component="p" gutterBottom>
+//         © {new Date().getFullYear()} Mohammad's Electronics E-Commerce. All
+//         rights reserved
+//       </Typography>
+//       <Box sx={{ display: "flex", justifyContent: "center", gap: 2, mt: 1 }}>
+//         <Link href="#" color="inherit" underline="hover">
+//           Privacy Policy
+//         </Link>
+//         <Link href="#" color="inherit" underline="hover">
+//           Terms of Service
+//         </Link>
+//         <Link href="/contact" color="inherit" underline="hover">
+//           Contact Us
+//         </Link>
+//       </Box>
+//     </Box>
+//   );
+// };
+
+// export default Footer;
+
+
+
+
+
 import React from "react";
 import { Box, Typography, Link } from "@mui/material";
 
